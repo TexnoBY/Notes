cb9090be0d35c253d58433654a9afed2bf97b384
TexnoBY <krivosheev2008@gmail.com>
2020-10-24 11:52:22 +0300

    NOTES-2 show notes in admin, note model

--- main_page/admin.py
+++ main_page/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'publish')
+    list_filter = ('author', 'created_at', 'updated_at', )
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title', 'body',)}
+    date_hierarchy = 'publish'
+    ordering = ('publish',)
+
+
--- main_page/models.py
+++ main_page/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Note(models.Model):
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    body = models.TextField()
+
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    publish = models.DateTimeField(default=timezone.now)
+
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='user_note')
+
+    def __str__(self):
+        return self.slug
