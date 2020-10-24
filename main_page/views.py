dae6d2b79916e04f1e2fd178853e592b75bef2c9
TexnoBY <krivosheev2008@gmail.com>
2020-10-24 17:01:51 +0300

    NOTES-3 all notes in main.html

--- main_page/views.py
+++ main_page/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
+
+
+def all_notes(request):
+    notes_lst = models.Note.objects.all()
+    return render(request,
+                  'main.html',
+                  {'notes': notes_lst})
