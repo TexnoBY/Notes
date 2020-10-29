762da8bc18f84ac75549a710b6afb07b6d5bb7ca
TexnoBY <krivosheev2008@gmail.com>
2020-10-29 14:14:13 +0300

    NOTES-7 registration

--- Notes/urls.py
+++ Notes/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.urls import include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_page.urls', namespace='main_page')),
 
+    path('', include('django_registration.backends.one_step.urls')),
+    path('', include('django.contrib.auth.urls')),
 ]
--- main_page/urls.py
+++ main_page/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
+
 from . import views
-from django.contrib.auth import views as auth_views
 
 app_name = 'main_page'
 
@@ -8,7 +8,5 @@ urlpatterns = [
     path('', views.NoteListView.as_view(), name='all_notes'),
     path('<int:note_id>/', views.delete_note, name='delete_note'),
     path('add/', views.add_note, name='add_note'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
